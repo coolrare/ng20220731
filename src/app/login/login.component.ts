11ec9883ed1397d0f9cf2b7a17ed211c6ae0c036
Mike Huang <wellwind@mail2000.com.tw>
2022-07-31 15:14:26 +0800

    示範任務 04

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,36 @@
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+
+    this.loginService.login(this.user).subscribe({
+      next: (result) => {
+        console.log(result);
+        localStorage.setItem('token', result.user.token);
+        // this.router.navigate(['/']);
+        this.router.navigateByUrl('/');
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.error.body);
+      }
+    });
+
   }
-
 }
